4d8344607b29c027fe33b719d58012864efa0751
ErickZH <zaraterick@outlook.com>
2020-11-25 16:18:11 -0600

    Test styling

--- src/Weight.php
+++ src/Weight.php
@@ -1,19 +1,20 @@
 <?php
 
 namespace Erickzh\UnitConversions;
+use Exception;
 
 class Weight
 {
     private float $kilograms;
 
-    public static function fromKilograms(float $kilograms): self
-    {
+    public static function fromKilograms(float $kilograms): self {
         return new static($kilograms);
     }
 
     public function __construct(float $kilograms)
     {
         $this->kilograms = $kilograms;
+        
     }
 
     public function toLbs(): float
